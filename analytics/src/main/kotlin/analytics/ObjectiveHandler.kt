e3ef9380d9173dcb886ef1aa4093454e7ed54a95
Martin Drozdik <drozdik.svk@gmail.com>
2018-04-13 17:53:34 +0200

    2

--- analytics/src/main/kotlin/analytics/ObjectiveHandler.kt
+++ analytics/src/main/kotlin/analytics/ObjectiveHandler.kt
@@ -1,7 +1,7 @@
 package analytics
 
 
-//import backend.server.events.*
+import backend.server.events.*
 import org.springframework.data.mongodb.core.index.Indexed
 import org.springframework.data.mongodb.core.mapping.Document
 import org.springframework.data.repository.reactive.ReactiveCrudRepository
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Mono
-
+/*
 sealed class ServerEvent
 
 @Document
@@ -20,7 +20,7 @@ data class UserRegistered
     @Indexed val something: String
 
 ): ServerEvent()
-
+*/
 @Repository
 interface ServerEventRepository: ReactiveCrudRepository<ServerEvent, String>
 
